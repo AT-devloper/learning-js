bba3359bde72fd13e8e48b75ae2a9051dd9510d8
AT-developer <ayushap.poddar@gmail.com>
2025-02-22 16:36:17 +0530

    si

---
+++ 22feb.js
@@ -0,0 +1,7 @@
+let M = document.getElementById("I")
+M.src="lap2.jpg"
+ 
+
+let text = document.getElementById("well")
+text.style.color ="red"
+text.style.textShadow="0px 0px 9px blue"
---
+++ tesk.js
@@ -0,0 +1,7 @@
+let pe = document.getElementById("text")
+pe.innerHTML= "<i> Magni delectus quas </i>"
+pe.style.textShadow ="0px 0px 5px red"
+let but = document.getElementById("btn")
+but.style.background="indianred"
+but.style.padding = "10px"
+but.style.boxShadow="0px 0px 5px 5px #333"

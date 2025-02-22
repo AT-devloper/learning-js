da8758854656150574d842a564e6224a0962a5ec
AT-developer <ayushap.poddar@gmail.com>
2025-02-22 19:08:13 +0530

    22feb

--- tesk.js
+++ tesk.js
@@ -1,7 +1,11 @@
 let pe = document.getElementById("text")
 pe.innerHTML= "<i> Magni delectus quas </i>"
 pe.style.textShadow ="0px 0px 5px red"
+pe.style.color ="#fff"
+
 let but = document.getElementById("btn")
 but.style.background="indianred"
 but.style.padding = "10px"
 but.style.boxShadow="0px 0px 5px 5px #333"
+but.style.borderRadius="20px"
+but.style.fontSize="20px"

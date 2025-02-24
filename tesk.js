828e43db329993f8df4df312d8d503de5cd16247
at developer <ayushap.poddar@gmail.com>
2025-02-24 10:14:55 +0530

    24feb

--- tesk.js
+++ tesk.js
@@ -1,7 +1,12 @@
+
+
 let pe = document.getElementById("text")
-pe.innerHTML= "<i> Magni delectus quas </i>"
+pe.innerHTML= "<b> Magni delectus quas </b>"
 pe.style.textShadow ="0px 0px 5px red"
 pe.style.color ="#fff"
+pe.style.letterSpacing="5px"
+pe.style.fontFamily="Cinzel, serif"
+pe.style.fontSize="50px"
 
 let but = document.getElementById("btn")
 but.style.background="indianred"
@@ -9,3 +14,8 @@ but.style.padding = "10px"
 but.style.boxShadow="0px 0px 5px 5px #333"
 but.style.borderRadius="20px"
 but.style.fontSize="20px"
+but.style.textShadow="0px 0px 5px #fff"
+
+let img=document.getElementById("photo")
+img.style.borderRadius="20px"
+img.style.boxShadow="0px 0px 20px 2px blue"

826f26707a37e21e98f87e237abe8ae4f13eea14
at developer <ayushap.poddar@gmail.com>
2025-02-25 16:29:44 +0530

    25feb

---
+++ 25feb.js
@@ -0,0 +1,39 @@
+let but = document.getElementById("btn")
+but.style.background="indianred"
+but.style.padding = "10px"
+but.style.boxShadow="0px 0px 8px 5px aqua"
+but.style.borderRadius="20px"
+but.style.fontSize="20px"
+but.style.textShadow="0px 0px 5px #fff"
+
+let b = document.getElementById("bt")
+b.style.background="green"
+b.style.padding = "10px"
+b.style.boxShadow="0px 0px 8px 5px red"
+b.style.borderRadius="20px"
+b.style.fontSize="20px"
+b.style.textShadow="0px 0px 5px #fff"
+
+let n = document.getElementById("nt")
+n.style.background="aqua"
+n.style.padding = "10px"
+n.style.boxShadow="0px 0px 8px 5px blue"
+n.style.borderRadius="20px"
+n.style.fontSize="20px"
+n.style.textShadow="0px 0px 5px #fff"
+
+let c = document.getElementById("ct")
+c.style.background="gray"
+c.style.padding = "10px"
+c.style.boxShadow="0px 0px 8px 5px green"
+c.style.borderRadius="20px"
+c.style.fontSize="20px"
+c.style.textShadow="0px 0px 5px #fff"
+
+function fun(){
+    alert("Server busy") //left click
+}
+
+function leave(){
+    alert("try again") //left click
+}
---
+++ tsk.js
@@ -0,0 +1,17 @@
+function k(){
+    let p = document.getElementById('hero')
+    p.textContent="ZERO"
+}
+
+function r(){
+    let bol = document.body         
+    bol.setAttribute("bgcolor","red")
+}
+function b(){
+    let bol = document.body         
+    bol.setAttribute("bgcolor","blue")
+}
+function y(){
+    let bol = document.body         
+    bol.setAttribute("bgcolor","yellow")
+}

254fbc20a63b96529e02b15163d1f285590ab5d6
at developer <ayushap.poddar@gmail.com>
2025-02-28 16:33:02 +0530

    28feb

---
+++ 28feb.js
@@ -0,0 +1,7 @@
+let me = document.querySelector(".head")
+me.innerHTML = "<i>YOU ARE WELLCOME</i>"
+me.style.color ="blue"
+
+
+let list = document.querySelectorAll("li")
+list[2].style.color="red"
---
+++ que.js
@@ -0,0 +1,36 @@
+var name = "ayush"
+console.log(name)
+
+let a = parseInt(prompt('1 NUMBER'))
+let d = parseInt(prompt('2 number'))
+let c =("")
+
+c = a
+a = d
+d = c
+
+console.log(a,"<br>")
+console.log(d)
+
+
+
+let num1 = parseInt(prompt("enter number for evenodd"))
+
+if (num1%2==0){
+    console.log("even")
+}
+
+else{
+    console.log("odd numder")
+}
+
+
+
+let age = parseInt(prompt("Enter age "))
+
+if ( age >=18){
+   document.write("you are now voter")
+}
+else { 
+    document.write("You are child ")
+}

a4a4449534845a5f99536f3d35351ca2480ecc1c
at developer <ayushap.poddar@gmail.com>
2025-03-01 16:18:55 +0530

    1mar

---
+++ 1mar.js
@@ -0,0 +1,13 @@
+// let a = (b,c)=>{return b+c}
+// document.write(a(5,8))
+
+
+// let u = (parseInt(prompt("Enter a Redius  = ")))
+// let a = (u)=>{return (3.14*u*u) }
+// document.write(a(u))
+
+// let c = (a,b) => (a*b)
+// document.write(c(4,5))
+
+let f = a => (a*a)
+document.write(f(30))

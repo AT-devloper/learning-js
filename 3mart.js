e569816f4214a6cb28b4b160f36825510abd0a6a
at developer <ayushap.poddar@gmail.com>
2025-03-03 23:48:32 +0530

    3mar

---
+++ 3mart.js
@@ -0,0 +1,12 @@
+var count = 0;
+function add(){
+    if (count<4){
+    let el =document.createElement("Input")
+    let bol=document.body
+    bol.body =document.append(el)
+    count++;
+    }
+    else {
+        alert("limit are crossed")
+    }
+}  

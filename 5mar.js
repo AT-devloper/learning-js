cbc79a6407790cd77bf7f1a4f359a7774c19c9b3
at developer <ayushap.poddar@gmail.com>
2025-03-05 16:31:55 +0530

    5mar

---
+++ 5mar.js
@@ -0,0 +1,23 @@
+// // setTimeout(() => {
+// //    alert("LOGIN for more ") 
+// // }, 3000);
+
+// function fun(){
+// let a=document.getElementById("text")
+// a.style.display="none"
+// }
+
+// setTimeout(() => {document.write()
+    
+// }, 5000);
+
+function data(){
+    let num1 = parseInt(document.querySelector('#num1').value)
+    let num2 = parseInt(document.getElementById('num2').value)
+    let a = document.querySelector("#ans")
+    a.innerHTML= num1+num2
+
+    return false
+
+}
+

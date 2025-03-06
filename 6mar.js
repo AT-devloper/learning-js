365aa68b7fe43ff6e68228bb4259c5aa508b2b71
at developer <ayushap.poddar@gmail.com>
2025-03-06 16:31:31 +0530

    6mar

---
+++ 6mar.js
@@ -0,0 +1,39 @@
+function validate(){
+    let name =document.querySelector('#name').value
+    let email =document.querySelector('#email').value
+    let age =document.querySelector('#age').value
+
+    if(name==""){               //.focus are predefinfunctio
+        // alert("Enter your name")
+        let ero = document.querySelector('#errorname')
+        ero.innerHTML="please enter name"
+        document.querySelector('#name').focus() 
+        return false
+    }
+    else if(email==""){
+        alert("Enter your email")
+        document.querySelector('#email').focus()
+        return false
+    }
+    else if(!(email.includes('@gmail.com'))){
+        alert("Enter email with @gmail.com")
+        document.querySelector('#email').focus()
+        return false
+    }
+
+
+    else if(age==""){
+        alert("Enter your age")
+        document.querySelector('#age').focus()
+        return false 
+    }
+    else if(isNaN(age)){
+        alert("Enter your age in number")
+        document.querySelector('#age').focus()
+        return false 
+    }
+
+
+
+
+}

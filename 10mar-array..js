93350a86d1f5e59de23ade5179d4828f334b7d7c
at developer <ayushap.poddar@gmail.com>
2025-03-10 16:30:21 +0530

    10mar

---
+++ 10mar-array..js
@@ -0,0 +1,18 @@
+let arr = [12,44.4,"A",'cybrom',true]
+
+for (let vl of arr){
+    console.log(vl);
+}
+
+for ( let k in arr){
+    console.log(k);
+}
+
+// input element form user in ARRAY
+
+let el = parseInt(prompt("Enter 5  elements"))
+let ar=[]
+
+for (let vl of ar){
+    console.log(vl);
+}

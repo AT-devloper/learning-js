e997b31f6542d4eee19ba4f9f2c755fe27a8c410
at developer <ayushap.poddar@gmail.com>
2025-03-10 15:59:31 +0530

    22feb

---
+++ 10mar.js
@@ -0,0 +1,14 @@
+let btn= document.querySelector('#btn')
+btn.addEventListener('click',()=>{alert("GREETING")})
+
+
+let btn1= document.querySelector('#btn1')
+btn1.addEventListener('dblclick',()=>{alert("HELLO")})
+
+
+let btn2= document.querySelector('#btn2')
+btn2.addEventListener('mouseenter',()=>{alert("WELCOME")})
+
+
+let btn3= document.querySelector('#btn3')
+btn3.addEventListener('mouseleave',()=>{alert("OK")})

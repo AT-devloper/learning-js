ff5c24ba2e59985f7b7260e4600d8d6d23084a5a
at developer <ayushap.poddar@gmail.com>
2025-03-11 09:28:05 +0530

    11mar

--- 10mar-array..js
+++ 10mar-array..js
@@ -1,18 +1,16 @@
-let arr = [12,44.4,"A",'cybrom',true]
+// let arr = [12,44.4,"A",'cybrom',true]
+// for (let vl of arr){
+//     console.log(vl);
+// }
+// for ( let k in arr){
+//     console.log(k);
+// }
 
-for (let vl of arr){
-    console.log(vl);
+
+let size = parseInt(prompt("Enter size = "));// input element form user in ARRAY
+let input = new Array(Number(size));
+for(let i=0;i<size;i++){
+    input[i]=prompt('Enter element : ');
 }
-
-for ( let k in arr){
-    console.log(k);
-}
-
-// input element form user in ARRAY
-
-let el = parseInt(prompt("Enter 5  elements"))
-let ar=[]
-
-for (let vl of ar){
-    console.log(vl);
-}
+console.log("Elements are - ",(input));
+// console.log(input); 

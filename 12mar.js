d0b6b06c1230548c2ba7a2eea038c9d8b448da02
at developer <ayushap.poddar@gmail.com>
2025-03-12 21:37:37 +0530

    12mar

---
+++ 12mar.js
@@ -0,0 +1,29 @@
+// let aar = [1,2,3,4,5]
+
+// aar.push("last element") //add element in last
+// console.log(aar);
+
+// aar.unshift("fisrt element","second element") //add element on start
+// console.log(aar);
+
+// aar.pop() //remove last element
+// console.log(aar) 
+
+// aar.shift() //remove first element
+// console.log(aar)
+
+// let arr=["AYUSH" , 23 , 1234567 , "bhopal" , "java"]
+
+// let exect = arr.slice(1,4)
+// console.log(exect);
+
+let ar=[1,2,3,45,6,7,89,10,34,76,87]        //H.W
+let el=ar.slice(1,9)
+for(let vl of el){
+    if(vl%2==0){
+        console.log("even = ", vl );
+    }
+}
+
+
+

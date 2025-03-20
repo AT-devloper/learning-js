4d3541a10baf3d4c6af646927e51a78e87a110be
at developer <ayushap.poddar@gmail.com>
2025-03-20 16:30:11 +0530

    20mar

---
+++ 20mar.js
@@ -0,0 +1,12 @@
+let obj={
+    name:"ayush",
+    city:"bpl",
+    age:23,
+    contact:1234567890,
+    account:{
+        balance:20000,
+        address:"bairaghar"
+    }
+}
+console(obj.name);
+// console.log(obj.account.balance );
---
+++ objt.js
@@ -0,0 +1,13 @@
+function getdata(){ 
+let person={
+    Name : document.querySelector('#Name').value,
+    Address:document.querySelector('#Address').value,
+    Contect:document.querySelector('#Contect').value,
+    Age:document.querySelector('#Age').value
+}
+
+    console.log(person);
+
+    return false
+
+}

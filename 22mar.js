9cf6832c5db4e401a3b31e1fdaeaddc82637fcb0
at developer <ayushap.poddar@gmail.com>
2025-03-24 09:45:53 +0530

    22mar

---
+++ 22mar.js
@@ -0,0 +1,49 @@
+// let ob ={
+//     name :"ayush",
+//     contect : 1234,
+//     abfun:function(){
+//         alert("working")
+//     },
+//     car:["audi","bmw","royal"]
+// }
+// ob.abfun()
+// console.log(ob.car[0]);
+
+
+
+// let ob1={
+//     name:"ayush",
+//     age:23,
+//     contect:1234
+// }
+// let ob2={
+//     srname:"poddar",
+//     city:"bhopal",
+//     state:"MP",
+//     ...ob1
+// }
+// // let ob3={...ob1,...ob2}
+
+// ob2.city="indore"
+// ob2.email="ayush@gmail.com"
+
+// delete ob2.srname
+// console.log(ob2)
+
+let person={
+    name:"ayush",
+    city:"bpl",
+    contect:"1234"
+}
+
+// for (let k in person){
+//     console.log(k);
+// }
+for(let b in person){   //for keys of object
+    console.log(person[b]);
+}
+let c = Object.keys(person) //for single key 
+console.log(c[1]);
+
+let o = Object.values(person) //for single value
+document.write(o[2]);

0645a4acc4cca4ea46c97829682da3e54d58c858
at developer <ayushap.poddar@gmail.com>
2025-03-24 16:26:37 +0530

    24mar

--- 22mar.js
+++ 22mar.js
@@ -1,49 +1,53 @@
-// let ob ={
-//     name :"ayush",
-//     contect : 1234,
-//     abfun:function(){
-//         alert("working")
-//     },
-//     car:["audi","bmw","royal"]
-// }
-// ob.abfun()
-// console.log(ob.car[0]);
+// // let ob ={
+// //     name :"ayush",
+// //     contect : 1234,
+// //     abfun:function(){
+// //         alert("working")
+// //     },
+// //     car:["audi","bmw","royal"]
+// // }
+// // ob.abfun()
+// // console.log(ob.car[0]);
 
 
 
-// let ob1={
+// // let ob1={
+// //     name:"ayush",
+// //     age:23,
+// //     contect:1234
+// // }
+// // let ob2={
+// //     srname:"poddar",
+// //     city:"bhopal",
+// //     state:"MP",
+// //     ...ob1
+// // }
+// // // let ob3={...ob1,...ob2}
+
+// // ob2.city="indore"
+// // ob2.email="ayush@gmail.com"
+
+// // delete ob2.srname
+// // console.log(ob2)
+
+// let person={
 //     name:"ayush",
-//     age:23,
-//     contect:1234
+//     city:"bpl",
+//     contect: 1234
 // }
-// let ob2={
-//     srname:"poddar",
-//     city:"bhopal",
-//     state:"MP",
-//     ...ob1
+
+// // for (let k in person){
+// //     console.log(k);
+// // }
+// for(let b in person){   //for keys of object
+//     console.log(person[b]);
 // }
-// // let ob3={...ob1,...ob2}
+// let c = Object.keys(person) //for single key 
+// console.log(c[1]);
 
-// ob2.city="indore"
-// ob2.email="ayush@gmail.com"
+// let o = Object.values(person) //for single value
+// document.write(o[2]);
 
-// delete ob2.srname
-// console.log(ob2)
+let {name,age,contact}={name:"ayush",age:23,contact:23456}
 
-let person={
-    name:"ayush",
-    city:"bpl",
-    contect: 1234
-}
-
-// for (let k in person){
-//     console.log(k);
-// }
-for(let b in person){   //for keys of object
-    console.log(person[b]);
-}
-let c = Object.keys(person) //for single key 
-console.log(c[1]);
-
-let o = Object.values(person) //for single value
-document.write(o[2]);
+console.log(name);
---
+++ 24mar_module.js
@@ -0,0 +1,11 @@
+// main file
+
+// import {add as plus,mul as multi} from './fun1.js'
+
+import plus from './fun1.js'   //default direct name change
+
+let ans= plus(10,10)
+console.log(ans);
+
+let answ =multi(20,20)
+console.log(answ);

012d54f9e726b0490d616e949d986ad5feaa3df7
at developer <ayushap.poddar@gmail.com>
2025-03-27 16:33:33 +0530

    27mar

---
+++ 27marstri.js
@@ -0,0 +1,21 @@
+//string
+
+let st ="welcome"
+console.log(st[6]);
+
+
+//template literals
+
+let name = "Ayush"
+
+let nam1 =`my name is ${name}`
+
+console.log(nam1);
+
+//functions -> toupercase ,tolowercase .
+
+let address = "bhopal"
+console.log(address.toUpperCase());
+
+let repeat = address.repeat(2)
+console.log(repeat); 

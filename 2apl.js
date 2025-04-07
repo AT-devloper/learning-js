7512f208f678e71ff08c4320218debe1c2fdcd3e
AT-devloper <ayushap.poddar@gmail.com>
2025-04-07 22:28:51 +0530

    7apl

--- 2apl.js
+++ 2apl.js
@@ -18,22 +18,48 @@ let fetchData= async()=>{
         <td>${e.age}</td>
         <td>${e.contact}</td>
         <td>${e.person}</td>
-        <td>${e.price}</td>
+        <td>${e.price*e.person}</td>
         <td onclick="Del('${e.id}')"> Delete </td>
         
         </tr>
         
-        `
-    })
+        `}
+    )}
 
-}
     let Del=(id)=>{
             let url=`http://localhost:3000/hotel/${id}`
-
             fetch(url,{method:"DELETE"})
     }
 
 
+    let Book=()=>{
+        let inname = document.querySelector('#name').value; 
+        let inage = document.querySelector("#age").value;
+        let incont = document.querySelector("#cont").value;
+        let inper = document.querySelector("#per").value;
+        
+        // let inadd=document.querySelector("#add").value
+        // let incountry=document.querySelector("#country").value
+    }
 
+        let url="http://localhost:3000/hote"
 
-fetchData()
+        fetch(url ,
+            {
+            method :"POST",
+            headers :{
+                "Content-Type":"appliction/json"
+            },
+            body : JSON.stringify({
+                    "name": inname ,
+                    "age": inage,
+                    "contact": incont,
+                    "person": inper,
+                    // "address":inadd,
+                    // "country":incountry
+                })
+        })
+
+        location.href="2apl.html"
+        return false
+//  fetchData()
